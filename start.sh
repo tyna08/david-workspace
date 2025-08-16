d7eb62d7bd4d6ff7898421298178962b45f6db2f
tyna08 <lanedatechnology@gmail.com>
2025-08-16 12:41:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo 'Starting deployment...'
-cd backend
-echo 'Installing backend dependencies...'
+echo "Starting deployment..."
+cd /opt/render/project/src/backend
+echo "Current directory:"
+pwd
+echo "Checking if models exist:"
+ls -la models/
+echo "Installing backend dependencies..."
 npm install --production
-echo 'Dependencies installed, starting server...'
+echo "Dependencies installed, starting server..."
 node server.js
